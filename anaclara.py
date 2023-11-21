4fd13f3691edf32239dab75c2efa9c58f2de46cf
ana27clara06 <147432834+ana27clara06@users.noreply.github.com>
2023-11-21 16:50:50 -0300

    Add files via upload

---
+++ anaclara.py
@@ -0,0 +1,19 @@
+def soma (n1,n2):
+    soma = n1+n2
+    print(soma)
+def subtração(n1,n2):
+    subtração = n1-n2
+    print(subtração)
+def multiplicação(n1,n2):
+    multiplicação = n1*n2 
+    print(multiplicação)
+def divisão(n1,n2):
+    divisão = n1/n2
+    print(divisão)
+
+n1 = (int(input("digite um número:")))
+n2 = (int(input("digite um número:")))
+soma(n1,n2)
+subtração(n1,n2)
+multiplicação(n1,n2)
+divisão(n1,n2)
